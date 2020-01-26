0e66d3397178b6245838ae0460c0ab007684b463
timgass <timothyagass@gmail.com>
2020-01-26 10:03:28 -0600

    initial commit

---
+++ build/precache-manifest.ebe139efed4a356ab0758110f2bc0900.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18c6458c00ec73af0025f003ec80fc3",
+    "url": "/web-frontend/index.html"
+  },
+  {
+    "revision": "c1801bbf7eb75b2f0bf9",
+    "url": "/web-frontend/static/css/main.0fbd37dc.chunk.css"
+  },
+  {
+    "revision": "4dbd6085336a05032efc",
+    "url": "/web-frontend/static/js/2.1b598bbf.chunk.js"
+  },
+  {
+    "revision": "9fbcf26faf345f33038f61e1827d66ff",
+    "url": "/web-frontend/static/js/2.1b598bbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1801bbf7eb75b2f0bf9",
+    "url": "/web-frontend/static/js/main.5078688a.chunk.js"
+  },
+  {
+    "revision": "42518cce3ec0eb4ddf29",
+    "url": "/web-frontend/static/js/runtime-main.f2035e96.js"
+  },
+  {
+    "revision": "ff7abf12f0496ac5a9d38932c850caca",
+    "url": "/web-frontend/static/media/map.ff7abf12.png"
+  }
+]);
